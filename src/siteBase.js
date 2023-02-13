6b78729b0705d40b3ac17c79f62fd72b05d45e32
Anthony <cooperkl317@gmail.com>
2023-02-12 16:05:08 -0800

    add buttons to render different pages of site

--- src/siteBase.js
+++ src/siteBase.js
@@ -1,42 +1,51 @@
+import { renderHome } from "./home";
+import { renderContact } from "./contact";
+import { renderMenu } from "./menu";
+
+const baseContent = document.getElementById('base-content');
+const pageContent = document.getElementById('pages');
 
 function renderHeader() {
-
-    const content = document.getElementById('content');
-
-    const header = document.createElement('div');
+    const header = document.createElement('h2');
     header.classList.add('header');
-    header.innerHTML = 'Maikham Thai & Lao';
+    header.textContent = 'Maikham Thai & Lao';
 
-    content.appendChild(header);
-
+    baseContent.appendChild(header);
 }
 
 function renderNav() {
-    const content = document.getElementById('content');
-
-    const nav = document.createElement('div');
+    const nav = document.createElement('nav');
     nav.classList.add('nav');
 
     const homeButton = document.createElement('button');
     homeButton.classList.add('btn');
-    homeButton.innerHTML = "Home";
+    homeButton.textContent = "Home";
+    homeButton.addEventListener('click', () => {
+        pageContent.innerHTML = '';
+        renderHome();
+    });
 
     const menuButton = document.createElement('button');
     menuButton.classList.add('btn');
-    menuButton.innerHTML = "Menu";
+    menuButton.textContent = "Menu";
+    menuButton.addEventListener('click', () => {
+        pageContent.innerHTML = '';
+        renderMenu();
+    });
 
     const contactButton = document.createElement('button');
     contactButton.classList.add('btn');
-    contactButton.innerHTML = "Contact";
+    contactButton.textContent = "Contact";
+    contactButton.addEventListener('click', () => {
+        pageContent.innerHTML = '';
+        renderContact();
+    });
 
     nav.appendChild(homeButton);
     nav.appendChild(menuButton);
     nav.appendChild(contactButton);
 
-
-
-    content.appendChild(nav);
-
+    baseContent.appendChild(nav);
 }
 
 export { renderHeader, renderNav }
