a73d1c029d16ca60e20d7f10cdb21025c21b77ee
Anthony <cooperkl317@gmail.com>
2023-02-12 16:05:37 -0800

    render home page when opening site

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import { renderHome } from "./home";
 import { renderHeader, renderNav } from "./siteBase";
+import { renderHome } from "./home";
+import { renderContact } from "./contact";
+import { renderMenu } from "./menu";
 
 renderHeader();
 renderNav();
 renderHome();
+
+
 
